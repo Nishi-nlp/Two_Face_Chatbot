da2b185178a371453a15b110f5920d0de6c22527
Nishi <59068467+Nishi-nlp@users.noreply.github.com>
2025-04-22 19:27:47 +0900

    Update streamlit_app.py

--- dev/streamlit_app.py
+++ dev/streamlit_app.py
@@ -34,7 +34,8 @@ async def main():
     with torch.no_grad():
         outputs = model(**inputs)
         logits = outputs.logits
-
+        print(outputs.logits)
+        
     predicted_class_id = torch.argmax(logits, dim=1).item()
 
     id2label = {
